01e8a5bee3cd41db233d4989f229b38dc66c94e8
kevinzwm1 <kevin.zwm@gmail.com>
2016-04-24 10:06:37 +1000

    Added my project

---
+++ src/main/java/com/kevinzhao/IoneApplication.java
@@ -0,0 +1,12 @@
+package com.kevinzhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoneApplication.class, args);
+	}
+}
